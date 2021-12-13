bed7d817fb3335796f8092edab5bad9ace825f15
HiromitsuYasu <1843105s@stu.kobe-u.ac.jp>
2021-12-13 12:41:47 +0900

    behind y=0.9

--- calculate/theta_y_fixed.py
+++ calculate/theta_y_fixed.py
@@ -17,7 +17,7 @@ def theta(Q2, y):
     return a
 
 cordinate_1 = np.arange(0.001, 0.99, 0.001)
-"""
+
 X_1 = cordinate_1
 Y_1 = theta(cordinate_1, 0.1)
 Y_2 = theta(cordinate_1, 0.2)
@@ -28,6 +28,7 @@ Y_6 = theta(cordinate_1, 0.6)
 Y_7 = theta(cordinate_1, 0.7)
 Y_8 = theta(cordinate_1, 0.8)
 Y_9 = theta(cordinate_1, 0.9)
+"""
 plt.plot(X_1, Y_1, label="y = 0.1")
 plt.plot(X_1, Y_2, label="y = 0.2")
 plt.plot(X_1, Y_3, label="y = 0.3")
@@ -36,9 +37,10 @@ plt.plot(X_1, Y_5, label="y = 0.5")
 plt.plot(X_1, Y_6, label="y = 0.6")
 plt.plot(X_1, Y_7, label="y = 0.7")
 plt.plot(X_1, Y_8, label="y = 0.8")
-plt.plot(X_1, Y_9, label="y = 0.9")
+#plt.plot(X_1, Y_9 / np.pi, label="y = 0.9")
 plt.legend()
 plt.xlabel('Q^2 [Gev^2]', fontsize=14)
+plt.ylabel("rad")
 plt.title('theta fixed y')
 plt.show()
 """
