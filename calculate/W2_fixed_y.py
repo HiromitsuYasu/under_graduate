3dc4cc94b5e6bf4f0c3394677d80483b583e9a90
HiromitsuYasu <1843105s@stu.kobe-u.ac.jp>
2022-01-21 22:27:42 +0900

    calculate W^2

--- calculate/W2_fixed_y.py
+++ calculate/W2_fixed_y.py
@@ -1,6 +1,4 @@
 import numpy as np
-from mpl_toolkits.mplot3d.axes3d import Axes3D
-import math
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import W2
@@ -9,32 +7,30 @@ E_mu = 1.5
 m_mu = 0.1057
 p_mu = np.sqrt(E_mu **2 - m_mu **2)
 
-cordinate_1 = np.arange(0.001, 0.99, 0.001)
+Q2_range_min = 0.143
+Q2_range_max = 0.99
+cordinate_1 = np.linspace(Q2_range_min, Q2_range_max, 1000)
 
 X_2 = cordinate_1
-Y_01 = W2.W2(X_2, 0.1)
-Y_02 = W2.W2(X_2, 0.2)
-Y_03 = W2.W2(X_2, 0.3)
-Y_04 = W2.W2(X_2, 0.4)
-Y_05 = W2.W2(X_2, 0.5)
-Y_06 = W2.W2(X_2, 0.6)
-Y_07 = W2.W2(X_2, 0.7)
-Y_08 = W2.W2(X_2, 0.8)
-Y_09 = W2.W2(X_2, 0.9)
+Y_02 = np.sqrt(W2.W2(X_2, 0.2))
+Y_025 = np.sqrt(W2.W2(X_2, 0.25))
+Y_03 = np.sqrt(W2.W2(X_2, 0.3))
+Y_033 = np.sqrt(W2.W2(X_2, 0.33))
 
-plt.plot(X_2, Y_01, label="y = 0.1")
+
+fig = plt.figure()
 plt.plot(X_2, Y_02, label="y = 0.2")
+plt.plot(X_2, Y_025, label = "y = 0.25" )
 plt.plot(X_2, Y_03, label="y = 0.3")
-plt.plot(X_2, Y_04, label="y = 0.4")
-plt.plot(X_2, Y_05, label="y = 0.5")
-plt.plot(X_2, Y_06, label="y = 0.6")
-plt.plot(X_2, Y_07, label="y = 0.7")
-plt.plot(X_2, Y_08, label="y = 0.8")
-plt.plot(X_2, Y_09, label="y = 0.9")
+plt.plot(X_2, Y_033, label="y = 0.33")
+plt.hlines(1.08, 0, 1, label = "W = 1.08 GeV")
+
+plt.ylim(1.0, 1.3)
 
 plt.legend()
 plt.xlabel('Q^2 [Gev^2]', fontsize=14)
 plt.ylabel("W [GeV]")
 plt.title('W fixed y')
 plt.grid()
-plt.show()
+
+fig.savefig("./graph/W2_fixed_y.png")
