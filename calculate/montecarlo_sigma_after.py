4f8e84460184c7f0a3224ca6a4b65daa16b54366
HiromitsuYasu <1843105s@stu.kobe-u.ac.jp>
2022-01-21 22:26:49 +0900

    unknown, probably garbage

--- calculate/montecarlo_sigma_after.py
+++ calculate/montecarlo_sigma_after.py
@@ -3,19 +3,36 @@ import math
 import matplotlib.pyplot as plt
 import sigma_used_book 
 import theta_y_fixed 
+import ROOT as r
+from copy import copy
+r.gROOT.SetBatch()
 
-Q2_rand = 0.001 + (0.99 - 0.001) * np.random.rand(100000)
-y_rand = 0.001 + (0.99 - 0.001) * np.random.rand(100000)
-cross_section = sigma_used_book.crosssection(y_rand, Q2_rand)
-theta = theta_y_fixed.theta(y_rand, Q2_rand)
+def montecarlo_sigma_after():
+    Q2_rand = 0.001 + (0.99 - 0.001) * np.random.rand(10**7)
+    y_rand = 0.001 + (0.99 - 0.001) * np.random.rand(10**7)
+    cross_section = sigma_used_book.crosssection(y_rand, Q2_rand)
+    theta = theta_y_fixed.theta(y_rand, Q2_rand)
 
+    hist_root = r.TH1D("theta", "used structure function directly;theta [rad];event", 100, 0, 0.5)
+    hist_root.FillN(10**6, np.nan_to_num(theta), np.nan_to_num(cross_section))
+    for i in range(hist_root.GetNbinsX()): hist_root.SetBinError(i, 0)
+    hist_root.SetBinContent(1, 0)
+    canvas = r.TCanvas()
+    hist_root.Draw("hist")
+    canvas.SaveAs("theta_distribution_after.png")
+    return copy(hist_root)
+
+montecarlo_sigma_after()
+    
+"""
 plt.hist(
     theta/np.pi,
     weights=cross_section,
     range=(0, 1.0),
-    bins=30,
+    bins= 30,
     )
 plt.xlabel("theta [rad/pi]")
 plt.ylabel("proportional to prob")
-plt.title("after")
+plt.title("structure")
 plt.show()
+"""
--- calculate/montecarlo_sigma_before.py
+++ calculate/montecarlo_sigma_before.py
@@ -3,12 +3,28 @@ import math
 import matplotlib.pyplot as plt
 import def_crosssection_Q2 
 import theta_y_fixed 
+import ROOT as r
+from copy import copy
+r.gROOT.SetBatch()
 
-Q2_rand = 0.001 + (0.99 - 0.001) * np.random.rand(100000)
-y_rand = 0.001 + (0.99 - 0.001) * np.random.rand(100000)
-cross_section = def_crosssection_Q2.crosssection(y_rand, Q2_rand)
-theta = theta_y_fixed.theta(y_rand, Q2_rand)
+def montecarlo_sigma_before():
+    Q2_rand = 0.001 + (0.99 - 0.001) * np.random.rand(10**7)
+    y_rand = 0.001 + (0.99 - 0.001) * np.random.rand(10**7)
+    cross_section = def_crosssection_Q2.crosssection(y_rand, Q2_rand)
+    theta = theta_y_fixed.theta(y_rand, Q2_rand)
 
+    hist_root = r.TH1D("theta", "used flux method;theta [rad];event", 100, 0, 0.5)
+    hist_root.FillN(10**6, np.nan_to_num(theta), np.nan_to_num(cross_section))
+    for i in range(hist_root.GetNbinsX()): hist_root.SetBinError(i, 0)
+    hist_root.SetBinContent(1, 0)
+    canvas = r.TCanvas()
+    hist_root.Draw("hist")
+    canvas.SaveAs("theta_distribution_before.png")
+    return copy(hist_root)
+
+montecarlo_sigma_before()
+
+"""
 plt.hist(
     theta/np.pi,
     weights=cross_section,
@@ -17,5 +33,6 @@ plt.hist(
     )
 plt.xlabel("theta [rad/pi]")
 plt.ylabel("proportional to prob")
-plt.title("before")
+plt.title("flux")
 plt.show()
+"""
