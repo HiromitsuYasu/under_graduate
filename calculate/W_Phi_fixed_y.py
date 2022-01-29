93000da61f172650ce0da5cee3f6a7b966d2d383
HiromitsuYasu <1843105s@stu.kobe-u.ac.jp>
2022-01-29 17:22:33 +0900

    calculate Phi

--- calculate/W_Phi_fixed_y.py
+++ calculate/W_Phi_fixed_y.py
@@ -1,39 +1,27 @@
 import numpy as np
-import math
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import W_Phi
+import function_for_angle_distribution
+import function_for_sigma
 
-E_mu = 1.5
-m_mu = 0.1057
-p_mu = np.sqrt(E_mu **2 - m_mu **2)
+E_gamma_range_min = 0.3
+E_gamma_range_max = 0.5
 
-cordinate_1 = np.arange(0.001, 0.99, 0.001)
+division = 5
+y_range_min = function_for_sigma.calculate_y_from_E_gamma(E_gamma_range_min)
+y_range_max = function_for_sigma.calculate_y_from_E_gamma(E_gamma_range_max)
+y = np.linspace(y_range_min, y_range_max, division)
 
-X_2 = cordinate_1
-Y_01 = W_Phi.Phi(X_2, 0.1)
-Y_02 = W_Phi.Phi(X_2, 0.2)
-Y_03 = W_Phi.Phi(X_2, 0.3)
-Y_04 = W_Phi.Phi(X_2, 0.4)
-Y_05 = W_Phi.Phi(X_2, 0.5)
-Y_06 = W_Phi.Phi(X_2, 0.6)
-Y_07 = W_Phi.Phi(X_2, 0.7)
-Y_08 = W_Phi.Phi(X_2, 0.8)
-Y_09 = W_Phi.Phi(X_2, 0.9)
+fig = plt.figure()
 
-plt.plot(X_2, Y_01, label="y = 0.1")
-plt.plot(X_2, Y_02, label="y = 0.2")
-plt.plot(X_2, Y_03, label="y = 0.3")
-plt.plot(X_2, Y_04, label="y = 0.4")
-plt.plot(X_2, Y_05, label="y = 0.5")
-plt.plot(X_2, Y_06, label="y = 0.6")
-plt.plot(X_2, Y_07, label="y = 0.7")
-plt.plot(X_2, Y_08, label="y = 0.8")
-#plt.plot(X_2, Y_09, label="y = 0.9")
+for y_value in y:
+    Q2_min = function_for_sigma.calculate_Q2_min_from_y(y_value)
+    Q2_max = function_for_sigma.calculate_Q2_max_from_y(y_value)
+    Q2 = np.linspace(Q2_min, Q2_max, division * 10)
+    plt.plot(Q2, function_for_angle_distribution.Phi(y_value, Q2), label = "y = {}".format(y_value))
 
 plt.legend()
-plt.xlabel('Q^2 [Gev^2]', fontsize=14)
+plt.xlabel('Q^2 [Gev^2]')
 plt.ylabel("rad")
 plt.title('Phi fixed y')
 plt.grid()
-plt.show()
+fig.savefig("./graph/Phi_fixed_y.png")
